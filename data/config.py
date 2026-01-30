7468ee67f24b7d19e082128f8766e7104321895b
Nodirbek2000-tat <nodirbekshukurov382@gmail.com>
2026-01-30 20:52:57 +0500

    eee

--- data/config.py
+++ data/config.py
@@ -18,6 +18,6 @@ DB_HOST = env.str("DB_HOST")
 DB_PORT = env.str("DB_PORT")
 DB_NAME = env.str("DB_NAME")
 DB_USER = env.str("DB_USER")
-DB_PASSWORD = env.str("DB_PASSWORD")
+DB_PASSWORD = env.str("DB_PASS")
 
 
